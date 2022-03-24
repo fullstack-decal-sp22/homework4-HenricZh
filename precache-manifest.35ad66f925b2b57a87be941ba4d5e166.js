609f5182d432f2bc599e5c913d06b98b6790f9a4
Henric Zhang <henriczhang@berkeley.edu>
2022-03-24 16:34:15 -0700

    Updates

---
+++ precache-manifest.35ad66f925b2b57a87be941ba4d5e166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2a080b94c92fba2096",
+    "url": "/homework4-HenricZh/static/css/main.be2e60e2.chunk.css"
+  },
+  {
+    "revision": "ce2a080b94c92fba2096",
+    "url": "/homework4-HenricZh/static/js/main.ce2a080b.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/homework4-HenricZh/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "0075cb693e9567ceee3b",
+    "url": "/homework4-HenricZh/static/js/runtime~main.0075cb69.js"
+  },
+  {
+    "revision": "f4d64c5104637a80dca5fe7624b02a8a",
+    "url": "/homework4-HenricZh/index.html"
+  }
+];
